7cfdba6b8a078faeea052d0e9d2577a23692e9f0
Christian Knieling <cknieling@outlook.com>
2020-12-30 15:37:07 +0100

    added code to support separate Autoencoder for feet

--- Helpers/DataPreprocessor.py
+++ Helpers/DataPreprocessor.py
@@ -6,29 +6,33 @@ from scipy.spatial.transform import Rotation as R
 from glob import glob
 from os import listdir
 from os.path import isfile, join, isdir
+import pandas as pd
 
 def resample_mocap_data(target_delta_t, in_delta_t, data):
     nr_data_points = data.shape[0]
-    animation_duration =  nr_data_points * in_delta_t
+    animation_duration = nr_data_points * in_delta_t
     in_axis = np.linspace(start=0, stop=animation_duration, num=nr_data_points)
-    target_axis = np.linspace(start=0, stop=animation_duration, num=np.floor(animation_duration / target_delta_t).astype(np.int))
+    target_axis = np.linspace(start=0, stop=animation_duration,
+                              num=np.floor(animation_duration / target_delta_t).astype(np.int))
     sampler = sciInterp.interp1d(in_axis, data, axis=0)
     return sampler(target_axis)
 
 
-def reshape_for_cnn( data):
+def reshape_for_cnn(data):
     data = data.reshape(data.shape[0], -1, 6)
     data = np.swapaxes(data, 1, 2)
     return data
+
 
 def reshape_from_cnn(data):
     data = np.swapaxes(data, 1, 2)
     data = data.reshape(data.shape[0], -1)
     return data
 
+
 def augment_dataset(data, nr_of_angles):
-    #1 -> 180 degree rotation
-    #2 -> 2x 120x rotation
+    # 1 -> 180 degree rotation
+    # 2 -> 2x 120x rotation
     angle_step = 360.0 / float(nr_of_angles)
 
     output = data
@@ -41,18 +45,21 @@ def augment_dataset(data, nr_of_angles):
     return output
 
 
-
 class ParalellMLPProcessor():
     def __init__(self, nr_of_timesteps_per_feature, target_delta_T, augment_rotation_number):
         self.nr_of_timesteps_per_feature = nr_of_timesteps_per_feature
         self.target_delta_t = target_delta_T
         self.inputs = np.array([])
         self.outputs = np.array([])
+        self.feet_inputs = np.array([])
+        self.feet_outputs = np.array([])
         self.heads = np.array([])
         self.min = 1.0
         self.max = 1.0
         self.input_scaler = None
         self.output_scaler = None
+        self.feet_input_scaler = None
+        self.feet_output_scaler = None
         self.augment_rotation_number = augment_rotation_number
 
     def append_file(self, file_name):
@@ -67,17 +74,20 @@ class ParalellMLPProcessor():
         frame_time = mocap_importer.frame_time
         bone_dependencies = mocap_importer.bone_dependencies
 
-        resampled_global_pos = resample_mocap_data(in_delta_t=frame_time, target_delta_t=self.target_delta_t, data=global_positions.reshape(global_positions.shape[0], -1))
+        resampled_global_pos = resample_mocap_data(in_delta_t=frame_time, target_delta_t=self.target_delta_t,
+                                                   data=global_positions.reshape(global_positions.shape[0], -1))
 
         resampled_global_pos = resampled_global_pos.reshape(resampled_global_pos.shape[0], -1, 3)
-        resampled_global_pos = augment_dataset(resampled_global_pos.reshape(-1,3), self.augment_rotation_number).reshape(-1, resampled_global_pos.shape[1], 3)
+        resampled_global_pos = augment_dataset(resampled_global_pos.reshape(-1, 3),
+                                               self.augment_rotation_number).reshape(-1, resampled_global_pos.shape[1],
+                                                                                     3)
 
-        head_idx    = joint_names.index('Head')
-        l_hand_idx  = joint_names.index('LeftHand')
-        r_hand_idx  = joint_names.index('RightHand')
-        l_foot_idx  = joint_names.index('LeftFoot')
-        r_foot_idx  = joint_names.index('RightFoot')
-        hip_idx     = joint_names.index('Hips')
+        head_idx = joint_names.index('Head')
+        l_hand_idx = joint_names.index('LeftHand')
+        r_hand_idx = joint_names.index('RightHand')
+        l_foot_idx = joint_names.index('LeftFoot')
+        r_foot_idx = joint_names.index('RightFoot')
+        hip_idx = joint_names.index('Hips')
         l_shoulder_idx = joint_names.index('LeftArm')
         r_shoulder_idx = joint_names.index('RightArm')
         l_elbow_idx = joint_names.index('LeftForeArm')
@@ -89,38 +99,57 @@ class ParalellMLPProcessor():
         heads = resampled_global_pos[:, [head_idx], :]
         resampled_global_pos -= resampled_global_pos[:, [head_idx] * number_of_limbs, :]
 
-        set = resampled_global_pos[:, [l_hand_idx, r_hand_idx, l_shoulder_idx, r_shoulder_idx, hip_idx, l_foot_idx, r_foot_idx, l_elbow_idx, r_elbow_idx, l_knee_idx, r_knee_idx],:]
-        inputs = set[:, [0, 1], :]
-        outputs = set[:, [2, 3, 4, 5, 6, 7, 8, 9, 10], :]
+        ff_set = resampled_global_pos[:,
+                 [l_hand_idx, r_hand_idx, l_shoulder_idx, r_shoulder_idx, hip_idx, l_foot_idx, r_foot_idx, l_elbow_idx,
+                  r_elbow_idx, l_knee_idx, r_knee_idx], :]
+        hand_inputs = ff_set[:, [0, 1], :]
+        skeletal_outputs = ff_set[:, [2, 3, 4, 5, 6, 7, 8, 9, 10], :]
 
-        inputs = inputs.reshape(inputs.shape[0], -1)
-        outputs = outputs.reshape(outputs.shape[0], -1)
+        ae_set = resampled_global_pos[:,
+                 [l_hand_idx, r_hand_idx, hip_idx, l_foot_idx, r_foot_idx, l_knee_idx, r_knee_idx], :]
+
+        feet_inputs = ae_set.reshape(ae_set.shape[0], -1)
+        feet_outputs = ae_set[:, [2, 3, 4, 5, 6], :].reshape(ae_set.shape[0], -1)
+
+        hand_inputs = hand_inputs.reshape(hand_inputs.shape[0], -1)
+        skeletal_outputs = skeletal_outputs.reshape(skeletal_outputs.shape[0], -1)
         heads = heads.reshape(heads.shape[0], -1)
 
-        vels = np.diff(np.hstack((inputs, heads)), axis=0)
+        vels = np.diff(np.hstack((hand_inputs, heads)), axis=0)
         accels = np.diff(vels, axis=0)
 
-        rolled_inputs = inputs  # np.hstack((inputs,np.roll(inputs,-1, axis=0)))
+        rolled_hand_inputs = hand_inputs  # np.hstack((inputs,np.roll(inputs,-1, axis=0)))
+        rolled_feet_inputs = feet_inputs
         for i in range(1, self.nr_of_timesteps_per_feature):
-            rolled_inputs = np.hstack((rolled_inputs, np.roll(inputs, i * 1, axis=0)))
+            rolled_hand_inputs = np.hstack((rolled_hand_inputs, np.roll(hand_inputs, i * 1, axis=0)))
+            rolled_feet_inputs = np.hstack((rolled_feet_inputs, np.roll(feet_inputs, i * 1, axis=0)))
 
+        assert (self.nr_of_timesteps_per_feature >= 2)
         vels = vels[(self.nr_of_timesteps_per_feature - 1):, :]
         accels = accels[(self.nr_of_timesteps_per_feature - 2):, :]
 
-        rolled_inputs = rolled_inputs[self.nr_of_timesteps_per_feature:, :]
-        outputs = outputs[self.nr_of_timesteps_per_feature:, :]
+        rolled_feet_inputs = rolled_feet_inputs[self.nr_of_timesteps_per_feature:-1, :]
+        feet_outputs = feet_outputs[self.nr_of_timesteps_per_feature + 1:, :]
+
+        rolled_hand_inputs = rolled_hand_inputs[self.nr_of_timesteps_per_feature:, :]
+        skeletal_outputs = skeletal_outputs[self.nr_of_timesteps_per_feature:, :]
         heads = heads[self.nr_of_timesteps_per_feature:, :]
 
-        rolled_inputs = np.hstack((rolled_inputs, vels, accels))
+        rolled_hand_inputs = np.hstack((rolled_hand_inputs, vels, accels))
+        rolled_feet_inputs = np.hstack((rolled_feet_inputs, vels[:-1, :], accels[:-1, :]))
 
-        if self.inputs.shape[0] ==  0:
-            self.inputs  = rolled_inputs
-            self.outputs = outputs
-            self.heads   = heads
+        if self.inputs.shape[0] == 0:
+            self.inputs = rolled_hand_inputs
+            self.outputs = skeletal_outputs
+            self.heads = heads
+            self.feet_inputs = rolled_feet_inputs
+            self.feet_outputs = feet_outputs
         else:
-            self.inputs  = np.vstack((self.inputs, rolled_inputs))
-            self.outputs = np.vstack((self.outputs, outputs))
-            self.heads   = np.vstack((self.heads, heads))
+            self.inputs = np.vstack((self.inputs, rolled_hand_inputs))
+            self.outputs = np.vstack((self.outputs, skeletal_outputs))
+            self.heads = np.vstack((self.heads, heads))
+            self.feet_inputs = np.vstack((self.feet_inputs, rolled_feet_inputs))
+            self.feet_outputs = np.vstack((self.feet_outputs, feet_outputs))
 
         self.__calc_scaling_fac()
 
@@ -152,8 +181,6 @@ class ParalellMLPProcessor():
         self.max = np.maximum(self.inputs.max(), self.outputs.max())
 
     def get_scaled_inputs(self, nr_of_angles=0):
-
-
         self.input_scaler = StandardScaler()
         self.input_scaler.fit(self.inputs)
         return self.input_scaler.transform(self.inputs)
@@ -162,6 +189,16 @@ class ParalellMLPProcessor():
         self.output_scaler = StandardScaler()
         self.output_scaler.fit(self.outputs)
         return self.output_scaler.transform(self.outputs)
+
+    def get_scaled_feet_inputs(self, nr_of_angles=0):
+        self.feet_input_scaler = StandardScaler()
+        self.feet_input_scaler.fit(self.feet_inputs)
+        return self.feet_input_scaler.transform(self.feet_inputs)
+
+    def get_scaled_feet_outputs(self):
+        self.feet_output_scaler = StandardScaler()
+        self.feet_output_scaler.fit(self.feet_outputs)
+        return self.feet_output_scaler.transform(self.feet_outputs)
 
     def scale_back_input(self, data):
         return self.input_scaler.inverse_transform(data)
@@ -175,10 +212,10 @@ class ParalellMLPProcessor():
     def scale_output(self, data):
         return self.output_scaler.transform(data)
 
-    def add_heads(self,data):
+    def add_heads(self, data):
         datacopy = data
         for i in range(data.shape[1]):
-            datacopy[:,i,:] += self.heads
+            datacopy[:, i, :] += self.heads
         return datacopy
 
     def get_min(self):
@@ -191,6 +228,8 @@ class ParalellMLPProcessor():
         np.savez(target_dir,
                  inputs=self.inputs,
                  outputs=self.outputs,
+                 feet_inputs=self.feet_inputs,
+                 feet_outputs=self.feet_outputs,
                  nr_of_timesteps_per_feature=self.nr_of_timesteps_per_feature,
                  target_delta_t=self.target_delta_t,
                  heads=self.heads,
@@ -201,6 +240,8 @@ class ParalellMLPProcessor():
 
         self.inputs = numpy_importer['inputs']
         self.outputs = numpy_importer['outputs']
+        self.feet_inputs = numpy_importer['feet_inputs']
+        self.feet_outputs = numpy_importer['feet_outputs']
         self.nr_of_timesteps_per_feature = numpy_importer['nr_of_timesteps_per_feature']
         self.target_delta_t = numpy_importer['target_delta_t']
         self.heads = numpy_importer['heads']
---
+++ MLPTAE.py
@@ -0,0 +1,281 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.onnx
+from glob import glob
+from os import listdir
+from os.path import isfile, join
+
+from Helpers import MocapImporter
+from Helpers import Animator
+from Helpers import DataPreprocessor
+from sklearn.utils import shuffle
+from Helpers import TorchLayers
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+input_size = 6  # 28x28
+hidden_size = 350
+output_size = 27
+num_epochs = 60
+batch_size = 6000
+learning_rate = 0.0001
+STACKCOUNT = 10
+TARGET_FPS = 20.0
+# walking : 41_05
+
+
+training_prep = DataPreprocessor.ParalellMLPProcessor(STACKCOUNT, 1.0 / TARGET_FPS, 5)
+
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData_bvh", ["C:/Users/cknie/Desktop/convertedMocapData_bvh/Basketball/06_13.npz"])
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/MPI_HDM05")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/KIT", ["C:/Users/cknie/Desktop/convertedMocapData/KIT/dance_waltz12_poses.npz"])
+
+# training_prep.load_np('C:/Users/cknie/Desktop/convertedMocapData/all_BMLhandball_data.npz')
+
+training_prep.load_np('C:/Users/cknie/Desktop/convertedMocapData_bvh/all_2.npz')
+# training_prep.save('C:/Users/cknie/Desktop/convertedMocapData_bvh/all_2.npz')
+
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/ACCAD")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/BMLhandball")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/BMLmovi")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/DFaust_67")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/EKUT")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/Eyes_Japan_Dataset")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/HumanEva")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/Kit")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/MPI_HDM05")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/MPI_Limits")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/MPI_mosh")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/SFU")
+# training_prep.append_subfolders("C:/Users/cknie/Desktop/convertedMocapData/TotalCapture")
+
+
+eval_prep = DataPreprocessor.ParalellMLPProcessor(STACKCOUNT, 1.0 / TARGET_FPS, 1)
+eval_prep.append_file("C:/Users/cknie/Desktop/convertedMocapData_bvh/Basketball/06_13.npz")
+
+train_hands_input = training_prep.get_scaled_inputs()
+train_hands_output = training_prep.get_scaled_outputs()
+
+train_feet_input = training_prep.get_scaled_feet_inputs()
+train_feet_output = training_prep.get_scaled_feet_outputs()
+
+train_hands_input, train_hands_output = shuffle(train_hands_input, train_hands_output, random_state=42)
+train_feet_input, train_feet_output = shuffle(train_feet_input, train_feet_output, random_state=42)
+
+eval_input = eval_prep.get_scaled_inputs()  # .scale_input(eval_prep.inputs)
+eval_output = eval_prep.get_scaled_outputs()  # scale_output(eval_prep.outputs)
+
+input_size = train_hands_input.shape[1]
+feet_input_size = train_feet_input.shape[1]
+feet_output_size = train_feet_output.shape[1]
+# Fully connected neural network with one hidden layer
+
+class AENET(nn.Module):
+    def __init__(self, input_size, enc_dims, latent_dim, dec_dims, output_size):
+        super(AENET, self).__init__()
+        self.input_size = input_size
+
+        input_size_ = input_size
+        modules = []
+
+        for h_dim in enc_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(input_size_, out_features=h_dim),
+                    # nn.BatchNorm1d(h_dim),
+                    nn.LeakyReLU())
+            )
+            input_size_ = h_dim
+
+        modules.append(
+            nn.Sequential(
+                nn.Linear(input_size_, out_features=latent_dim),
+                # nn.BatchNorm1d(h_dim),
+                nn.LeakyReLU())
+        )
+
+        self.encoder = nn.Sequential(*modules)
+
+        # Build Decoder
+        modules = []
+
+        self.decoder_input = nn.Linear(latent_dim, dec_dims[0])
+
+        for i in range(len(dec_dims) - 1):
+            modules.append(
+                nn.Sequential(
+                    nn.Linear(dec_dims[i], out_features=dec_dims[i + 1]),
+                    # nn.BatchNorm1d(hidden_dims[i + 1]),
+                    nn.LeakyReLU())
+            )
+
+        self.decoder = nn.Sequential(*modules)
+
+        self.final_layer = nn.Sequential(
+            nn.Linear(dec_dims[-1], dec_dims[-1]),
+            # nn.BatchNorm1d(hidden_dims[-1]),
+            nn.LeakyReLU(),
+            nn.Linear(dec_dims[-1], output_size))
+
+    def forward(self, x):
+        latent = self.encoder(x)
+        dec_in = self.decoder_input(latent)
+        dec_out = self.decoder(dec_in)
+        out = self.final_layer(dec_out)
+        return out
+
+
+class FFNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(FFNet, self).__init__()
+        self.input_size = input_size
+        # self.noise = TorchLayers.GaussianNoise()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.relu2 = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.4)
+        self.l3 = nn.Linear(hidden_size, hidden_size)
+        self.relu3 = nn.ReLU()
+        self.l4 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h1 = self.l1(x)
+        # out = self.noise(out)
+        h1 = self.relu(h1)
+        h1 = self.l2(h1)
+        h1 = self.relu2(h1)
+        # out = self.dropout(out)
+        h1 = self.l3(h1)
+        h1 = self.relu3(h1)
+        out = self.l4(h1)
+
+        return out
+
+
+ff_model = FFNet(input_size, hidden_size, output_size).to(device)
+ae_model = AENET(feet_input_size, [64, 32, 16], 6, [16, 32, 64], feet_output_size).to(device)
+
+# Loss and optimizer
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(ff_model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0,
+                             amsgrad=False)
+
+feet_criterion = nn.MSELoss()
+feet_optimizer = torch.optim.Adam(ae_model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0,
+                             amsgrad=False)
+
+# Train the model
+n_total_steps = int(train_hands_input.shape[0] / batch_size)
+maxloss = 0.0
+test_maxloss = 0.0
+# train_hands_input = torch.tensor(train_hands_input).to(device).float()
+# train_hands_output = torch.tensor(train_hands_output).to(device).float()
+
+train_feet_input = torch.tensor(train_feet_input)
+train_feet_output = torch.tensor(train_feet_output)
+#
+# eval_input = torch.tensor(eval_input).to(device).float()
+# eval_output = torch.tensor(eval_output).to(device).float()
+
+
+# for epoch in range(num_epochs):
+#     for i in range(int(np.floor(train_hands_input.shape[0] / batch_size))):
+#         # origin shape: [100, 1, 28, 28]
+#         # resized: [100, 784]
+#
+#         input = train_hands_input[i * batch_size: (i + 1) * batch_size, :]
+#         target_output = train_hands_output[i * batch_size: (i + 1) * batch_size, :]
+#
+#         # Forward pass
+#         model_output = ff_model(input)
+#         loss = criterion(model_output, target_output)
+#
+#         # Backward and optimize
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         maxloss = np.fmax(maxloss, loss.item())
+#
+#         with torch.no_grad():
+#             ff_model.eval()
+#             model_eval_output = ff_model(eval_input)
+#             ff_model.train()
+#             test_loss = criterion(model_eval_output, eval_output)
+#             test_maxloss = np.fmax(test_maxloss, test_loss.item())
+#
+#         # if (i + 1) % 10 == 0:
+#         #     print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Train Loss: {maxloss:.8f}, Test Loss: {test_maxloss:.8f}')
+#         #     test_maxloss = 0.0
+#         #     maxloss = 0.0
+#
+#     if (epoch) % 10 == 0:
+#         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {maxloss:.8f}, Test Loss: {test_maxloss:.8f}')
+#         maxloss = 0.0
+#         test_maxloss = 0.0
+
+print("\n\nFEET:")
+maxloss = 0.0
+test_maxloss = 0.0
+for epoch in range(num_epochs):
+    for i in range(int(np.floor(train_feet_input.shape[0] / batch_size))):
+        # origin shape: [100, 1, 28, 28]
+        # resized: [100, 784]
+
+        input = train_feet_input[i * batch_size: (i + 1) * batch_size, :].to(device).float()
+        target_output = train_feet_output[i * batch_size: (i + 1) * batch_size, :].to(device).float()
+
+        # Forward pass
+        model_output = ae_model(input)
+        loss = feet_criterion(model_output, target_output)
+
+        # Backward and optimize
+        feet_optimizer.zero_grad()
+        loss.backward()
+        feet_optimizer.step()
+        maxloss = np.fmax(maxloss, loss.item())
+
+    if (epoch) % 10 == 0:
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {maxloss:.8f}, Test Loss: {test_maxloss:.8f}')
+        maxloss = 0.0
+        test_maxloss = 0.0
+
+# "l_hand_idx, r_hand_idx, l_elbow_idx, r_elbow_idx, hip_idx, l_foot_idx, r_foot_idx
+
+# [l_hand_idx, r_hand_idx, l_shoulder_idx, r_shoulder_idx, hip_idx, l_foot_idx, r_foot_idx, l_elbow_idx, r_elbow_idx, l_knee_idx, r_knee_idx]
+bone_dependencies = [[0, 7], [1, 8], [2, 4], [3, 4], [4, -1], [5, 9], [6, 10], [7, 2], [8, 3], [9, 4], [10, 4]]
+bone_dependencies = np.array(bone_dependencies)
+
+global_positions = np.hstack((training_prep.scale_back_input(eval_input.detach().cpu().numpy())[:, :6],
+                              training_prep.scale_back_output(target_output.detach().cpu().numpy())))
+# global_positions = np.hstack((eval_input, eval_output))
+global_positions = global_positions.reshape(global_positions.shape[0], -1, 3)
+global_positions = eval_prep.add_heads(global_positions)
+# training_prep.scale_back(global_positions)
+
+if __name__ == '__main__':
+    anim = Animator.MocapAnimator(global_positions, [''] * 40, bone_dependencies, 1.0 / TARGET_FPS)
+    anim.animation()
+
+# Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
+# with torch.no_grad():
+#     n_correct = 0
+#     n_samples = 0
+#     for input, output in test_loader:
+#         input = input.reshape(-1, 28 * 28).to(device)
+#         output = output.to(device)
+#         outputs = model(input)
+#         # max returns (value ,index)
+#         predicted = torch.round(10 * outputs.data).T
+#         n_samples += output.size(0)
+#         n_correct += (predicted == output).sum().item()
+#
+#     acc = 100.0 * n_correct / n_samples
+#     print(f'Accuracy of the network on the 10000 test images: {acc} %')
