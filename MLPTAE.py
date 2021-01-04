058adc69730b55c49a80418b85de37c72b42fdac
Christian Knieling <cknieling@outlook.com>
2021-01-04 13:05:57 +0100

    Fixed AE implementation and added changed bone positions to output

--- MLPTAE.py
+++ MLPTAE.py
@@ -17,12 +17,11 @@ from Helpers import TorchLayers
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 # Hyper-parameters
 input_size = 6  # 28x28
 hidden_size = 350
 output_size = 27
-num_epochs = 5
+num_epochs = 50
 batch_size = 6000
 learning_rate = 0.0001
 STACKCOUNT = 10
@@ -68,24 +67,29 @@ train_feet_output = training_prep.get_scaled_feet_outputs()
 train_hands_input, train_hands_output = shuffle(train_hands_input, train_hands_output, random_state=42)
 train_feet_input, train_feet_output = shuffle(train_feet_input, train_feet_output, random_state=42)
 
+train_feet_input = np.hstack((train_feet_input[:,0:15], train_feet_input[:,-24:]))
+
 eval_input = training_prep.scale_input(eval_prep.inputs)  # .scale_input(eval_prep.inputs)
 eval_output = training_prep.scale_output(eval_prep.outputs)  # scale_output(eval_prep.outputs)
 
+hands_input_size = 24
 input_size = train_hands_input.shape[1]
-feet_input_size = train_feet_input.shape[1]
+feet_input_size = train_feet_input.shape[1] - hands_input_size
 feet_output_size = train_feet_output.shape[1]
 # Fully connected neural network with one hidden layer
 
 class AENET(nn.Module):
-    def __init__(self, input_size, enc_dims, latent_dim, dec_dims, output_size):
+    def __init__(self, input_size, hands_size, enc_dims, latent_dim, dec_dims, output_size):
         super(AENET, self).__init__()
         self.input_size = input_size
 
-        input_size_ = input_size
-        modules = []
+        input_size_ = input_size + hands_size
+        input_size_hands_ = hands_size
+        lower_body_modules = []
+        input_modules = []
 
         for h_dim in enc_dims:
-            modules.append(
+            lower_body_modules.append(
                 nn.Sequential(
                     nn.Linear(input_size_, out_features=h_dim),
                     # nn.BatchNorm1d(h_dim),
@@ -93,29 +97,46 @@ class AENET(nn.Module):
             )
             input_size_ = h_dim
 
-        modules.append(
+        lower_body_modules.append(
             nn.Sequential(
                 nn.Linear(input_size_, out_features=latent_dim),
                 # nn.BatchNorm1d(h_dim),
                 nn.LeakyReLU())
         )
 
-        self.encoder = nn.Sequential(*modules)
+        for h_dim in enc_dims:
+            input_modules.append(
+                nn.Sequential(
+                    nn.Linear(input_size_hands_, out_features=h_dim),
+                    # nn.BatchNorm1d(h_dim),
+                    nn.LeakyReLU())
+            )
+            input_size_hands_ = h_dim
 
-        # Build Decoder
-        modules = []
+        input_modules.append(
+            nn.Sequential(
+                nn.Linear(input_size_hands_, out_features=latent_dim),
+                # nn.BatchNorm1d(h_dim),
+                nn.LeakyReLU())
+        )
 
-        self.decoder_input = nn.Linear(latent_dim, dec_dims[0])
+        self.lower_body_encoder = nn.Sequential(*lower_body_modules)
+        self.input_encoder = nn.Sequential(*input_modules)
+
+        print(self.input_encoder)
+        lower_body_modules = []
+
+        self.decoder_input = nn.Linear(latent_dim + hands_size, dec_dims[0])
 
         for i in range(len(dec_dims) - 1):
-            modules.append(
+            lower_body_modules.append(
                 nn.Sequential(
                     nn.Linear(dec_dims[i], out_features=dec_dims[i + 1]),
                     # nn.BatchNorm1d(hidden_dims[i + 1]),
                     nn.LeakyReLU())
             )
 
-        self.decoder = nn.Sequential(*modules)
+        self.decoder = nn.Sequential(*lower_body_modules)
 
         self.final_layer = nn.Sequential(
             nn.Linear(dec_dims[-1], dec_dims[-1]),
@@ -123,8 +144,10 @@ class AENET(nn.Module):
             nn.LeakyReLU(),
             nn.Linear(dec_dims[-1], output_size))
 
-    def forward(self, x):
-        latent = self.encoder(x)
+    def forward(self, lower_body, hands_input):
+        lower_latent = self.lower_body_encoder(torch.hstack((lower_body, hands_input)))
+        # hands_input_latent = self.input_encoder(hands_input)
+        latent = torch.hstack((lower_latent, hands_input))
         dec_in = self.decoder_input(latent)
         dec_out = self.decoder(dec_in)
         out = self.final_layer(dec_out)
@@ -158,9 +181,8 @@ class FFNet(nn.Module):
 
         return out
 
-
-ff_model = FFNet(input_size, hidden_size, output_size).to(device)
-ae_model = AENET(feet_input_size, [64, 32, 16], 6, [16, 32, 64], feet_output_size).to(device)
+ff_model = FFNet(input_size, hands_input_size, output_size).to(device)
+ae_model = AENET(feet_input_size, hands_input_size, [64, 32, 16], 6, [400, 400, 256, 64], feet_output_size).to(device)
 
 # Loss and optimizer
 criterion = nn.MSELoss()
@@ -170,6 +192,9 @@ optimizer = torch.optim.Adam(ff_model.parameters(), lr=learning_rate, betas=(0.9
 feet_criterion = nn.MSELoss()
 feet_optimizer = torch.optim.Adam(ae_model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-08, weight_decay=0,
                              amsgrad=False)
+
+
+to_numpy = lambda x : x.detach().cpu().numpy()
 
 # Train the model
 n_total_steps = int(train_hands_input.shape[0] / batch_size)
@@ -223,16 +248,18 @@ for epoch in range(num_epochs):
 print("\n\nFEET:")
 maxloss = 0.0
 test_maxloss = 0.0
-for epoch in range(num_epochs):
+for epoch in range(num_epochs * 2):
     for i in range(int(np.floor(train_feet_input.shape[0] / batch_size))):
         # origin shape: [100, 1, 28, 28]
         # resized: [100, 784]
 
         input = train_feet_input[i * batch_size: (i + 1) * batch_size, :].to(device).float()
         target_output = train_feet_output[i * batch_size: (i + 1) * batch_size, :].to(device).float()
+        input_feet = input[:, :-24]
+        input_hands = input[:, -24:]
 
         # Forward pass
-        model_output = ae_model(input)
+        model_output = ae_model(input_feet, input_hands)
         loss = feet_criterion(model_output, target_output)
 
         # Backward and optimize
@@ -250,22 +277,32 @@ with torch.no_grad():
     ff_model.eval()
     target_output = ff_model(eval_input)
 
-curr_input = torch.hstack((eval_input[:STACKCOUNT, :6], target_output[:STACKCOUNT, :9], target_output[:STACKCOUNT, 15:21]))
+STACKCOUNT = 1
+curr_input_mat = torch.hstack((target_output[:STACKCOUNT, 6:15], target_output[:STACKCOUNT, 21:27]))
+vels_and_accels = torch.hstack((eval_input[STACKCOUNT - 1, 0:6], eval_input[STACKCOUNT - 1, -18:]))
+new_input_feet = torch.flatten(curr_input_mat)
+
 lower_body_poses = None
 with torch.no_grad():
     for curr_eval_idx in range(STACKCOUNT, eval_input.shape[0]):
-        model_output = ae_model(curr_input)
+        model_output = ae_model(new_input_feet, vels_and_accels)
         if lower_body_poses is None:
             lower_body_poses = model_output
         else:
-            lower_body_poses = torch.vstack(lower_body_poses, model_output)
-        curr_input = torch.roll(curr_input, -1, 0)
-        curr_input[-1, :6] = eval_input[curr_eval_idx, :6]
-        curr_input[-1, 6:-18] = model_output
-        curr_input[-1, -18:] = eval_input[curr_eval_idx, -18:]
+            lower_body_poses = torch.vstack((lower_body_poses, model_output))
+
+        curr_input_mat = torch.roll(curr_input_mat, -1, 0)
+        vels_and_accels = torch.hstack((eval_input[curr_eval_idx, 0:6], eval_input[curr_eval_idx, -18:]))
+        curr_input_mat[-1] = model_output
+        curr_input_mat[-1][:3] = target_output[curr_eval_idx, 6:9]
+
+        new_input_feet = torch.flatten(curr_input_mat)
+
+target_output[STACKCOUNT:, 6:15] = lower_body_poses[:, :9]
+target_output[STACKCOUNT:, 21:27] = lower_body_poses[:, 9:]
 
 
-# "l_hand_idx, r_hand_idx, l_elbow_idx, r_elbow_idx, hip_idx, l_foot_idx, r_foot_idx
+        # "l_hand_idx, r_hand_idx, l_elbow_idx, r_elbow_idx, hip_idx, l_foot_idx, r_foot_idx
 
 # [l_hand_idx, r_hand_idx, l_shoulder_idx, r_shoulder_idx, hip_idx, l_foot_idx, r_foot_idx, l_elbow_idx, r_elbow_idx, l_knee_idx, r_knee_idx]
 bone_dependencies = [[0, 7], [1, 8], [2, 4], [3, 4], [4, -1], [5, 9], [6, 10], [7, 2], [8, 3], [9, 4], [10, 4]]
