b745922c1c8fb76ef28a1e2f4b5f192d4797d244
Christian Knieling <cknieling@outlook.com>
2021-01-25 08:53:38 +0100

    corrected input

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 from Helpers import DataPreprocessor
 from Helpers import ModelWrappers
+import sys
+
 
 STACKCOUNT = 15
 TARGET_FPS = 20
-take_last = lambda x : x.split('/')[-1]
+take_last = lambda x : "predictions/" + x.split('/')[-1].split('.')[0]
 
 eval_files = [  "E:/Master/Converted Mocap/Eyes_Japan_Dataset/hamada/greeting-01-hello-hamada_poses.npz",
                 "E:/Master/Converted Mocap/Eyes_Japan_Dataset/kudo/jump-12-boxer step-kudo_poses.npz",
-                "E:/Master/Converted Mocap/MPI_HDM05/dg/HDM_dg_03-01_03_120_poses.npz.npz",
-                "E:/Master/Converted Mocap/MPI_HDM05/tr/HDM_tr_04-01_01_120_poses.npz",
                 "E:/Master/Converted Mocap/KIT/576/MarcusS_AdrianM11_poses.npz",
-                "E:/Master/Converted Mocap/SFU/0015/0015_BasicKendo001_poses.npz",
                 "E:/Master/Converted Mocap/KIT/513/balance_on_beam06_poses.npz",
                 "E:/Master/Converted Mocap/Eyes_Japan_Dataset/hamada/gesture_etc-14-apologize-hamada_poses.npz",
                 "E:/Master/Converted Mocap/Eyes_Japan_Dataset/kanno/walk-01-normal-kanno_poses.npz",
@@ -21,25 +20,26 @@ eval_files = [  "E:/Master/Converted Mocap/Eyes_Japan_Dataset/hamada/greeting-01
 
 training_prep = DataPreprocessor.ParalellMLPProcessor(STACKCOUNT, 1.0 / TARGET_FPS, 5)
 
-# training_prep.append_subfolders("E:/Master/Converted Mocap/ACCAD")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/BMLhandball")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/BMLmovi")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/DFaust_67")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/EKUT")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/Eyes_Japan_Dataset")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/HumanEva")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/Kit")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/MPI_HDM05")
-# training_prep.append_subfolders("E:/Master/Converted Moca/MPI_Limits")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/MPI_mosh")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/SFU")
-# training_prep.append_subfolders("E:/Master/Converted Mocap/TotalCapture")
+# training_prep.append_subfolders("E:/Master/Converted Mocap/ACCAD", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/BMLhandball", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/BMLmovi", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/DFaust_67", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/EKUT", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/Eyes_Japan_Dataset", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/HumanEva", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/Kit", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/MPI_HDM05", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Moca/MPI_Limits", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/MPI_mosh", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/SFU", eval_files)
+# training_prep.append_subfolders("E:/Master/Converted Mocap/TotalCapture", eval_files)
 
-training_prep.load_np("E:/Master/Converted Mocap/combined.npz")
 
+training_prep.load_np("E:/Master/Converted Mocap/combined_without_eval.npz")
+# training_prep.append_file(eval_files[1])
 #FF
 ff_wrapper = ModelWrappers.ff_wrapper(training_prep)
-ff_wrapper.train(100, 10000, 0.0001)
+ff_wrapper.train(100, 20000, 0.0001)
 
 for file in eval_files:
     eval_prep = DataPreprocessor.ParalellMLPProcessor(STACKCOUNT, 1.0 / TARGET_FPS, 5)
@@ -48,7 +48,7 @@ for file in eval_files:
     ff_wrapper.save_prediction(take_last(file) + "_FF")
 #VAE
 vae_wrapper = ModelWrappers.vae_wrapper(training_prep)
-vae_wrapper.train(100, 90, 20000, 0.0001)
+vae_wrapper.train(5, 2, 20000, 0.0001)
 
 for file in eval_files:
     eval_prep = DataPreprocessor.ParalellMLPProcessor(STACKCOUNT, 1.0 / TARGET_FPS, 5)
@@ -64,9 +64,9 @@ for file in eval_files:
     eval_prep.append_file(file)
     rnn_wrapper.predict(eval_prep)
     rnn_wrapper.save_prediction(take_last(file) + "_RNN")
-#GLOW
+# GLOW
 glow_wrapper = ModelWrappers.glow_wrapper(training_prep)
-glow_wrapper.train(100, 90, 50, 0.0001)
+glow_wrapper.train(100, 50)
 
 for file in eval_files:
     eval_prep = DataPreprocessor.ParalellMLPProcessor(STACKCOUNT, 1.0 / TARGET_FPS, 5)
