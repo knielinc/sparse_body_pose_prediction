1b83a0cc6e3b777ba6904d865ed92fa9724534b6
Christian Knieling <cknieling@outlook.com>
2021-01-25 08:55:09 +0100

    added figure for distance between legs

--- Helpers/StatsPrinter.py
+++ Helpers/StatsPrinter.py
@@ -1,8 +1,30 @@
 import numpy as np
-
+import matplotlib
+import matplotlib.pyplot as plt
 TAB = "\t"
 
+def save_feet_distance(generated_data, reference_data, joint_names, activity):
+    t = np.arange(0.0, generated_data.shape[0], 1)
+
+    rl = joint_names.index('l_foot')
+    ll = joint_names.index('r_foot')
+
+    sample_feet_dists       = np.linalg.norm(generated_data[:,ll,:] - generated_data[:,rl,:], axis=1)
+    reference_feet_dists    = np.linalg.norm(reference_data[:,ll,:] - reference_data[:,rl,:], axis=1)
+    fig, ax = plt.subplots(figsize=(10, 3))
+    ax.plot(t, sample_feet_dists, color='green', label='estimated')
+    ax.plot(t, reference_feet_dists, color='red', label='ground_truth')
+
+    ax.legend()
+
+    ax.set(xlabel='time (s)', ylabel='distance (m)',
+           title='Distance between Feet over Time')
+    ax.grid()
+
+    fig.savefig(activity + ".png")
+
 def print_stats(generated_data, reference_data, joint_names, activity):
+    save_feet_distance(generated_data, reference_data, joint_names, activity)
     maxheight = np.min(reference_data[:,:,2]) * -1.7
     scaling = 1 # / maxheight
     norms = np.linalg.norm(scaling * (generated_data - reference_data), axis=2)
